3c42aef0304628cd3707c00362dcb91bb81bed7d
iegpeppino <iegpeppino@outlook.com.ar>
2025-08-02 15:44:20 +0000

    D7 (production V1.1): Correct logic in logger.go to prevent closing of the log file before any errors are logged

--- logger/logger.go
+++ logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"path/filepath"
 )
 
 var Logger *slog.Logger
@@ -10,16 +11,25 @@ var Logger *slog.Logger
 // This logger specifically logs errors occurring while getting system stats
 func SysDataLogger() {
 
+	// Create directory for logs if it doesn't exist
+	logPath := "./logs/errors/systemstats.log"
+	logDir := filepath.Dir(logPath)
+
+	err := os.MkdirAll(logDir, 0755)
+	if err != nil {
+		slog.Error("Failed to create log directory", "error", err)
+		Logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
+		return
+	}
+
 	// Configure logger to write to file
-	logFile, err := os.OpenFile("./logs/errors/systemstats.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logFile, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		slog.Error("Failed to open log file", "error", err)
 		// Return stderr of file loggin fails
 		Logger = slog.New(slog.NewJSONHandler(os.Stderr, nil))
 		return
 	}
-	// Close logfile
-	defer logFile.Close()
 
 	// Create handler with settings
 	handler := slog.NewJSONHandler(
